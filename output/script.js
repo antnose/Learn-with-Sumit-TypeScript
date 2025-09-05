d7797c75f7eb907f45d26ce154920133bd0a1067
antnose <antnose@gmail.com>
2025-09-05 18:20:58 +0600

    Basic type of typescript

---
+++ output/script.js
@@ -0,0 +1,16 @@
+// let playerName = "Mashrafi";
+// console.log(playerName);
+// playerName = 31;
+function multiply(a, b) {
+    return a * b;
+}
+// console.log(multiply(13, 4));
+// Array
+let fruits = ["apple", "banana", "orange"];
+// fruits.push(12) //It's give an error in code editor!
+console.log(fruits);
+let num = [];
+num.push(12, 21, 34);
+console.log(num);
+export {};
+//# sourceMappingURL=script.js.map
---
+++ src/script.ts
@@ -0,0 +1,19 @@
+// let playerName = "Mashrafi";
+// console.log(playerName);
+
+// playerName = 31;
+
+function multiply(a: number, b: number) {
+  return a * b;
+}
+
+// console.log(multiply(13, 4));
+
+// Array
+let fruits = ["apple", "banana", "orange"];
+// fruits.push(12) //It's give an error in code editor!
+console.log(fruits);
+
+let num: number[] = [];
+num.push(12, 21, 34);
+console.log(num);
